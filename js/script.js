2a4165b32f24308f9bf2a48e254c35b249df66b7
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2019-04-29 11:39:56 +0100

    Add raw wersion of modal

---
+++ js/script.js
@@ -0,0 +1,29 @@
+console.log("Hello world");
+
+/*		MODAL 		*/
+
+// Large images
+const imagesLinks = {
+	img01: "images/kodilla-certyficate-big.jpg"
+};
+// Get the modal
+var modal = document.getElementById('menu2Modal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  // modalImg.src = this.src;
+  modalImg.src = imagesLinks.img01;
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+};
