60c89874e7e546a6c126d7c8ef106f68a7fae126
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2019-11-30 13:53:41 +0000

    Working on sending message using php. The scrit reseves data by POST and set values in inputs

---
+++ js/menuScroll.js
@@ -0,0 +1,37 @@
+/*jshint esversion: 6 */ 
+  $("#home").click(() => {
+    $("html, body").animate(
+      {
+        scrollTop: $(".hero").offset().top
+      },
+      600
+    );
+  });
+
+  $("#aboutMe, #goDown").click(() => {
+    $("html, body").animate(
+      {
+        scrollTop: $(".tab-section").offset().top -60
+      },
+      600
+    );
+  });
+  $("#myProjects").click(() => {
+    $("html, body").animate(
+      {
+        scrollTop: $(".my-projects").offset().top -40
+      },
+      600
+    );
+  });
+    $("#contactMe").click(() => {
+      console.log('ccc');
+    $("html, body").animate(
+      {
+        scrollTop: $(".contact-me").offset().top
+      },
+      600
+    );
+  });
+
+  
---
+++ js/stickyNavbar.js
@@ -0,0 +1,41 @@
+var navbar = document.getElementById("topNavbar");
+var menuButtons = document.getElementsByClassName("main-menu");
+var navbarBrand = document.getElementById("navbarBrand");
+var myLogo = document.getElementById("myLogo");
+
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= 50) {
+//   	console.log('kki');
+//     navbar.classList.add("stick");
+//   } else {
+//     navbar.classList.remove("stick");
+//   }
+// }
+
+// navbar.classList.add("stick");
+
+
+var stickIt = function(){
+	  if (window.pageYOffset >= 100) {
+	  	myLogo.classList.remove("logo-up-animation");
+		myLogo.classList.add("my-logo", "animation");
+	    navbar.classList.add("stick", "animation");
+	    navbarBrand.classList.add("sticky-navbar-brand", "animation");
+		  for(var i=0; i<menuButtons.length; i++){
+		    	menuButtons[i].classList.add("sticky-buttons", "animation");	
+    	}
+  } else {
+    navbar.classList.remove("stick");
+    navbarBrand.classList.remove("sticky-navbar-brand");
+    myLogo.classList.remove("my-logo", "animation");
+    myLogo.classList.add("logo-up-animation");
+    for(var i2=0; i2<menuButtons.length; i2++){
+    	menuButtons[i2].classList.remove("sticky-buttons");	
+    }
+  }
+};
+
+window.addEventListener("scroll", stickIt);
+
